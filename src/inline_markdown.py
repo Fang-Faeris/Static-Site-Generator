7db63ed112ebcd6e849394752fe21bdef5972f42
Fang-Faeris <joshuajensen18@gmail.com>
2025-05-18 12:59:19 -0500

    regex is terrible

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,22 +1,33 @@
+import re
+
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
-    for node in olld_nodes:
+    for node in old_nodes:
         if node.text_type != text_type.TEXT:
             new_nodes.append(node)
             continue
         split_nodes = []
         sections = node.text.split(delimiter)
-        for section in sections:
-            if len(section) % 2 == 0:
-                raise Exception("Invalid Markdown, formatting needs to be closed.")
-            for i in range(0, len(section)):
-                if section[i] == "":
-                    continue
-                if section[i] % 2== 0:
-                    split_nodes.append(TextNode(section[i], text_type.TEXT))
-                else:
-                    split_nodes.append(TextNode(section[i], text_type))
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid Markdown, formatting needs to be closed.")
+        for i in range(0, len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], text_type.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
+
+def extract_markdown_images(text):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+def extract_markdown_links(text):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
